876d1dfccdbdf0e9773c5d3fc7f999e73fd371c0
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-07-25 11:18:49 +0100

    sql crud

--- NodeJS-03-SQL/session_3.sql
+++ NodeJS-03-SQL/session_3.sql
@@ -1,4 +1,4 @@
--- Active: 1721871947080@@127.0.0.1@3306
+-- Active: 1721880906665@@127.0.0.1@5432@test_db
 SELECT * FROM Album;
 SELECT * FROM "Artist";
 SELECT * FROM "Album" as alb INNER JOIN "Artist" as art ON alb."ArtistId" = art."ArtistId";
@@ -47,3 +47,45 @@ SELECT round(avg(Total),2) FROM "Invoice";
 --? LENGTH -- Karakter Sayısı (Kayıt sayısını tek satıra düşürmez. Her kaydın karakter sayısını aynı satıra yazar.)
 SELECT Title, length(Title) FROM Album;
 
+
+--------------! POSTGRES SQL !-------------
+
+SELECT * FROM Album;
+SELECT Title, length(Title) FROM Album;
+SELECT * FROM Invoice;
+
+--* USA'daki toplam fatura tutarı : 
+SELECT * FROM Invoice WHERE BillingCountry = 'USA';
+SELECT DISTINCT (BillingCountry) FROM Invoice;
+SELECT BillingCountry, count(InvoiceId) FROM Invoice GROUP BY BillingCountry;
+SELECT BillingCountry, 
+	    COUNT(InvoiceId) AS faturaSayisi, 
+	    SUM(Total) AS toplam, 
+	    MIN(Total) AS minimum, 
+	    MAX(Total) AS maximum,
+	    ROUND(AVG(Total), 2) AS ortalama
+    FROM Invoice
+    GROUP BY BillingCountry;
+
+SELECT Country, count(Country) AS "CountryCount"
+    FROM Customer
+    GROUP BY Country
+    HAVING count(Country) > 1;
+
+------------------------? CRUD ?--------------------------------
+--* insert --yeni kayıt >   Create
+--* select --sorgu >        Read
+--* update --güncelleme >   Update
+--* delete --silme >        Ddelete
+
+SELECT * FROM Genre;
+INSERT INTO Genre (GenreId, Name) VALUES (26, 'Halk Müziği'); -- Veriler 25 tane olduğu 25'te bittiği için 26 deriz.
+INSERT INTO Genre (GenreId, Name) VALUES(29, 'Türk Halk Müziği >'),(30, 'Pop Müziği >');
+INSERT INTO Genre (genreid,name) VALUES(27,'Turk Halk Muzigi'),(28,'Pop Muzigi');
+
+--Tehlikeli Komutlar (Delete, Update)
+UPDATE Genre SET Name = 'Türk Pop Müziği' WHERE GenreId = 30;
+DELETE FROM Genre WHERE GenreId = 29;
+UPDATE Track SET milliseconds = 0;
+DELETE FROM Genre;
+SELECT * FROM Track;
