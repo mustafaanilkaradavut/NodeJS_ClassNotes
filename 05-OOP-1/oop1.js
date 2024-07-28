1313e068b86ea0772a52affd0a9b1a465682f781
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-07-28 15:45:40 +0100

    namechange

---
+++ 05-OOP-1/oop1.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/*  -------------------------------------------------------------------------- */
+//?                                OOP: OBJECTS                                */
+/*  -------------------------------------------------------------------------- */
+
+// Direct obje isimlendirmede PascalCase veya camelCase yapı kullanırız.
+const exampleObject = {
+  propertyName: "value", // property, attribute, field
+  propertyArray: [],
+  propertyObject: {},
+
+  // Obje içerisindeki fonksiyonlara Method denir.
+  methodName: function () {
+    return "this is a method";
+  },
+  // Alternatif yazım:
+  methodAlternative() {
+    return "method-2";
+  },
+};
