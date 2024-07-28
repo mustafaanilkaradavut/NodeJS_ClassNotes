4e1599414bae45ed4c5617121343ca43cac00287
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-07-28 16:02:47 +0100

    object declaration

--- 05-OOP-1/oop1.js
+++ 05-OOP-1/oop1.js
@@ -19,3 +19,44 @@ const exampleObject = {
     return "method-2";
   },
 };
+
+// Nokta rotasyonu ile property çağırılabilir :
+// console.log(exampleObject.propertyName);
+// console.log(exampleObject.methodName());
+
+/* -------------------------------------------------------------------------- */
+//* Object Declaration :
+
+const Car = {
+  brand: "Ford",
+  model: "Mustang",
+  year: 1967,
+  isAutoGear: true,
+  colors: ["red", "white"],
+  details: {
+    color1: "red",
+    color2: "white",
+    engineSize: 5000,
+  },
+  startEngine: function (param = "1") {
+    console.log(param);
+    return "Engine started";
+  },
+};
+
+// console.log(Car.brand);
+// console.log(Car.colors);
+// console.log(Car.colors[1]);
+// console.log(Car.details);
+// console.log(Car.details.engineSize);
+// console.log(Car.startEngine());
+
+// Alternatif
+
+// console.log(Car["brand"]);
+// console.log(Car["colors"][1]);
+// console.log(Car["details"]["engineSize"]);
+// console.log(Car["startEngine"]());
+
+/* -------------------------------------------------------------------------- */
+
