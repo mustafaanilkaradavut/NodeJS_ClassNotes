1d3358c0ef38462c191c0712aaaa3dcbcd2fa79c
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-07-28 16:27:28 +0100

    array destructuring

--- 05-OOP-1/oop1.js
+++ 05-OOP-1/oop1.js
@@ -59,4 +59,64 @@ const Car = {
 // console.log(Car["startEngine"]());
 
 /* -------------------------------------------------------------------------- */
+//* "This" Keyword
 
+const Car2 = {
+  brand: "Fiat",
+  model: "Egea",
+  year: 2019,
+  isAutoGear: false,
+  colors: ["white", "grey"],
+  details: {
+    color1: "white",
+    color2: "grey",
+    engineSize: 1500,
+  },
+  startEngine: function (param = "1") {
+    console.log(param);
+    return "Engine started";
+  },
+
+  getDetails: function () {
+    // console.log(this);
+    // return this.details;
+    // return this.startEngine;
+    return (
+      this.brand + " " + this.model + " " + this.year + " " + this.startEngine()
+    );
+  },
+
+  // Arrow functionlar grlobal scope'tur. This kullanılamaz !
+  arrowMethod: () => {
+    return this;
+  },
+};
+
+// console.log(Car2.getDetails());
+// console.log(Car2.arrowMethod());
+
+/* -------------------------------------------------------------------------- */
+//* Array Destructuring
+
+const testArray = ["value0", "value1", "value2", "value3", "value4"];
+
+const test0 = testArray[0];
+const test1 = testArray[1];
+const test2 = testArray[2];
+const test3 = testArray[3];
+const test4 = testArray[4];
+const testPart = testArray.slice(3, 5);
+
+// console.log(test0, test1, test2, testPart);
+
+//__ Destructuring'te SIRALAMA önemlidir.
+// const [var0, var1, ...varPar] = testArray;
+// console.log(var0, var1, varPar);
+
+//* Rest Operator (...) - Toplayıcı Operatör (Eşittir ifadesinin sol tarafındaki REST operatördür.)
+const [var0, var1, ...varPar] = testArray;
+console.log(var0, var1, varPar);
+
+//* Spread Operatör ( Dağıtıcı/Serpiştirici ) ( Eşittir ifadesinin sağ tarafındaki spread operatörüdür.)
+const newArr = [...testArray, "value5", "value6"];
+console.log(newArr);
