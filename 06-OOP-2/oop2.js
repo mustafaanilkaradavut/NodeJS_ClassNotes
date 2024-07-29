dd75bcccb395f446c1b562be80933cd3791f9c62
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-07-29 03:36:43 +0100

    opp classes

---
+++ 06-OOP-2/oop2.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/* -------------------------------------------------------------------------- */
+//.. OOP: CLASSES
+/* -------------------------------------------------------------------------- */
+//? OOP: Object Oriented Programming
+//? DRY: Don't Repeat Yourself
+//? BLUEPRINT: Taslak (Mimarların kullandığı mavi şablon kağıdı)
+//? CLASS: Obje türetmek için kullanılacak şablon.
+/* -------------------------------------------------------------------------- */
+//* Class Declaration:
