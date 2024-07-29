5bf690bf0ee52186733264fb4c5cd5b569b88de9
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-07-29 03:53:21 +0100

    contructor

--- 06-OOP-2/oop2.js
+++ 06-OOP-2/oop2.js
@@ -3,9 +3,57 @@
 /* -------------------------------------------------------------------------- */
 //.. OOP: CLASSES
 /* -------------------------------------------------------------------------- */
-//? OOP: Object Oriented Programming
-//? DRY: Don't Repeat Yourself
-//? BLUEPRINT: Taslak (Mimarların kullandığı mavi şablon kağıdı)
-//? CLASS: Obje türetmek için kullanılacak şablon.
+//__ OOP: Object Oriented Programming
+//__ DRY: Don't Repeat Yourself
+//__ BLUEPRINT: Taslak (Mimarların kullandığı mavi şablon kağıdı)
+//__ CLASS: Obje türetmek için kullanılacak şablon.
 /* -------------------------------------------------------------------------- */
 //* Class Declaration:
+//? Class isimleri PascalCase yapıdadır.
+
+// class PascalNameCase{...}
+
+/* -------------------------------------------------------------------------- */
+//* Class Expression:
+
+const PascalNameCase = class {
+  undefinedProperty; // Sadece tanımlanıp bırakılabilir. (Değeri Undefined)
+  propertyName = "value"; // attribute, field
+
+  //__ Method tanımlarken başına function yazılmaz.
+  methodName() {
+    return "this is a method";
+  }
+
+  // Aşağıdaki yöntem class içinde geçerli değildir. Kullanılmaz.
+  // methodName = function () {};
+};
+
+//? INSTANCE = Bir class'tan türetilen objedir.
+//* Intance isimleride PascalName yapısındadır.
+
+const NewObj = new PascalNameCase();
+// console.log(NewObj);
+// console.log(NewObj.methodName());
+
+/* -------------------------------------------------------------------------- */
+//* Contructor Method:
+
+class Car {
+  isRunning = false;
+
+  constructor(brand, model, year = 1999) {
+    this.brand = brand;
+    this.model = model;
+    this.year = year;
+  }
+
+  runEngine() {
+    this.isRunning = true;
+    console.log("Engine Started");
+    return this.isRunning;
+  }
+}
+
+const Ford = new Car("Ford", "Mustang", 1967); //__ Buradaki Car() parantez contructor'u ifade ediyor.
+console.log(Ford);
