bfe792466e53fd19eb21233aafe6bf54cbe9382f
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-07-31 01:04:37 +0100

    export func

--- 07-NodeModules/module.js
+++ 07-NodeModules/module.js
@@ -4,8 +4,39 @@
 /* -------------------------------------------------------------------------- */
 //__ Doyayı içe aktarma:
 
-require("./modules/index");
-require("./modules/"); // default file name : index
+// require("./modules/index");
+// require("./modules/"); // default file name : index
 
 /* -------------------------------------------------------------------------- */
+//__ Import from Export
 
+// const test = require("./modules/");
+// test();
+
+// require("./modules")();
+
+/* -------------------------------------------------------------------------- */
+//__ Çoklu Function Kullanımı
+
+// const arrFunc = require("./modules");
+// console.log(arrFunc);
+// arrFunc[0]();
+// arrFunc[1]();
+// arrFunc[2]();
+
+//? Array Destructuring
+// const [test1, test2, test3] = require("./modules");
+// test1();
+// test2();
+// test3();
+
+// const objFunc = require("./modules/");
+// objFunc.test1();
+// objFunc.test2();
+// objFunc.test3();
+
+//? Object Destructuring
+const { test1, test3: func3, test2 } = require("./modules");
+test1();
+test2();
+func3();
--- 07-NodeModules/modules/index.js
+++ 07-NodeModules/modules/index.js
@@ -1,4 +1,44 @@
 "use strict";
 /* -------------------------------------------------------------------------- */
 
-console.log("Module index started");
+// console.log("Module index started");
+
+/* -------------------------------------------------------------------------- */
+//.. Export
+
+function test() {
+  console.log("Test function is working");
+}
+module.exports = test;
+
+//* Short Way
+// module.exports = function test() {
+//     console.log("Test function is working");
+//   }
+
+/* -------------------------------------------------------------------------- */
+
+function test1() {
+  console.log("Test-1 Working");
+}
+function test2() {
+  console.log("Test-2 Working");
+}
+function test3() {
+  console.log("Test-3 Working");
+}
+
+// module.exports = [test1, test2, test3]; //? Array Export -> Destructuring
+
+module.exports = {
+  test1: test1,
+  test2: test2,
+  test3: test3,
+};
+
+//? ShortHand:
+module.exports = {
+  test1,
+  test2,
+  test3,
+};
