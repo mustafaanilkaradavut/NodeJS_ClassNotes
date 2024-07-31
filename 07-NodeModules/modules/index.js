12f6597f83fc716b8c0619bc0f09db40b7b77192
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-07-31 01:10:32 +0100

    expression

--- 07-NodeModules/modules/index.js
+++ 07-NodeModules/modules/index.js
@@ -16,7 +16,7 @@ module.exports = test;
 //     console.log("Test function is working");
 //   }
 
-/* -------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- *
 
 function test1() {
   console.log("Test-1 Working");
@@ -28,17 +28,42 @@ function test3() {
   console.log("Test-3 Working");
 }
 
-// module.exports = [test1, test2, test3]; //? Array Export -> Destructuring
+module.exports = [test1, test2, test3]; //? Array Export -> Destructuring
 
 module.exports = {
-  test1: test1,
-  test2: test2,
-  test3: test3,
-};
+ test1: test1,
+ test2: test2,
+ test3: test3,
+ };
 
-//? ShortHand:
+? ShortHand:
 module.exports = {
   test1,
   test2,
   test3,
 };
+
+Object Export -2:
+ module.exports.test1 = test1
+ module.exports.test2 = test2
+ module.exports.test3 = test3
+
+/* -------------------------------------------------------------------------- */
+
+//.. Module export -> function expression
+
+// Object Export -3:
+
+module.exports.test1 = function () {
+  console.log("test1 working.");
+};
+
+module.exports.test2 = function () {
+  console.log("test2 working.");
+};
+
+module.exports.test3 = function () {
+  console.log("test3 working.");
+};
+
+module.exports.newVar = "new-value";
