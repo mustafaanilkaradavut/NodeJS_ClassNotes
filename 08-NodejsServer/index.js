3c82246916a47cfc757a57e7bef33af2daa6b230
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-08-01 17:44:16 +0100

    post-get

--- 08-NodejsServer/index.js
+++ 08-NodejsServer/index.js
@@ -7,7 +7,8 @@
 //? HTTP SERVER:
 
 const http = require("node:http"); // BuiltIn Module.
-/* -------------------------------------------------------------------------- */
+const { stringify } = require("node:querystring");
+/* -------------------------------------------------------------------------- *
 
 // http.createServer( (request, response)=>{...}) //* Sıralama önemlidir önce request sonra response gelir.
 
@@ -21,3 +22,64 @@ const app = http.createServer((request, response) => {
 // Default server (local domain) = localhost
 // Default server IP (local IP) = 128.0.0.1
 app.listen(8000, () => console.log("Server started: http://127.0.0.1:8000"));
+
+/* -------------------------------------------------------------------------- *
+
+const app = http.createServer((req, res) => {
+  if (req.url == "/") {
+    res.end("Main Page");
+  } else if (req.url == "/second") {
+    res.end("Second Page");
+  } else {
+    res.end("Any Page");
+  }
+});
+
+app.listen(8000, () => console.log("http://127.0.0.1:8000"));
+
+/* -------------------------------------------------------------------------- *
+
+const app = http
+  .createServer((req, res) => {
+    if (req.url == "/") {
+      res.end("Main Page");
+    } else if (req.url == "/second") {
+      res.end("Second Page");
+      // console.log(req);  //__ Detaylı bilgileri görmek için yazarız.
+    } else {
+      res.end("Any Page");
+    }
+  })
+  .listen(8000, () => console.log("http://127.0.0.1:8000"));
+
+/* -------------------------------------------------------------------------- */
+
+const app = http
+  .createServer((req, res) => {
+    if (req.url == "/api") {
+      // res.end("API"); //__ res.end işlemi blocklar. Devam eden kodlar çalışmaz.
+
+      // res.write("Writing - 1");
+      // res.write("Writing - 2");
+      // res.write("Writing - 3");
+      // res.write("Writing - 4");
+      // res.end();
+
+      // console.log(req.method); //__ GET, POST gibi işlemlerden hangisinin çalıştığını gösterir.
+
+      if (req.method == "GET") {
+        const obj = {
+          result: true,
+          message: "Hello World",
+        };
+        // res.write(obj); //* Bu şekilde yazamayız. Aşağıdaki gibi yapmalıyız.
+        res.write(JSON.stringify(obj));
+        res.end();
+      } else {
+        res.end("Wrong Method");
+      }
+    } else {
+      res.end("HTML");
+    }
+  })
+  .listen(8000, () => console.log("http://127.0.0.1:8000"));
