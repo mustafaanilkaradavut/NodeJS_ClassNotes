ed218d8f6d807cfca2dbad6e3f5c39da5224f933
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-08-02 17:54:28 +0100

    dotenv up

--- 09-Express_Routing/app.js
+++ 09-Express_Routing/app.js
@@ -3,4 +3,17 @@
 /* -------------------------------------------------------------------------- */
 //..                           EXPRESS & ROUTUNGS                             */
 /* -------------------------------------------------------------------------- */
-//__ npm i express dotenx
+
+//__ https://expressjs.com/
+//__ npm i express dotenv
+
+const express = require("express");
+const app = express();
+
+//? dotenv çalıştır :
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
+
+/* -------------------------------------------------------------------------- */
