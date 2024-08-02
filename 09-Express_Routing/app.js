35241567795e84f5999c1a95d06e8ad91ecf3d6a
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-08-02 18:03:06 +0100

    express url

--- 09-Express_Routing/app.js
+++ 09-Express_Routing/app.js
@@ -10,6 +10,30 @@
 const express = require("express");
 const app = express();
 
+//? Express olmadan URL sorgulama yöntemi aşağıdaki gibidir :
+// http.createServer((req, res) => {
+//     if (req.url = '/') {
+//         if (req.method == 'GET') {
+//         } else {
+//         }
+//     } else if (req.url = '/second') {
+//     }
+// })
+
+//? Express ile URL sorgulama :
+app.get("/", (req, res) => {
+  res.end("app.get is working");
+});
+app.post("/", (req, res) => {
+  res.end("app.post is working");
+});
+app.put("/", (req, res) => {
+  res.end("app.put is working");
+});
+app.delete("/", (req, res) => {
+  res.end("app.delete is working");
+});
+
 //? dotenv çalıştır :
 require("dotenv").config();
 const PORT = process.env.PORT || 8000;
