484d3531bfe4aaf9f1e1d70bb08fb0852fa834a5
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-08-04 19:43:42 +0100

    start project

---
+++ 10-Routes_Middlewares/router.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/* -------------------------------------------------------------------------- */
+//..                           EXPRESSJS - ROUTING                            */
+/* -------------------------------------------------------------------------- */
+
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+
+/* -------------------------------------------------------------------------- */
+//? "Router" is special app for URL control in ExpressJS.
+
+/* -------------------------------------------------------------------------- */
+app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
