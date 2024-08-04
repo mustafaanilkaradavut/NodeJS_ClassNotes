352fffa1234a5d3ff82dcda271408376952e5550
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-08-04 20:13:11 +0100

    router

--- 10-Routes_Middlewares/router.js
+++ 10-Routes_Middlewares/router.js
@@ -1,5 +1,4 @@
 "use strict";
-
 /* -------------------------------------------------------------------------- */
 //..                           EXPRESSJS - ROUTING                            */
 /* -------------------------------------------------------------------------- */
@@ -13,5 +12,62 @@ const PORT = process.env.PORT || 8000;
 /* -------------------------------------------------------------------------- */
 //? "Router" is special app for URL control in ExpressJS.
 
+// app.get("/", (req, res) => {
+//   res.send({
+//     message: "Hello World",
+//   });
+// });
+
+//__ express.Router():
+// const router = express.Router();
+
+// router.get("/", (req, res) => {
+//   res.send({
+//     message: "Home Page",
+//   });
+// });
+
+// router.get("/path", (req, res) => {
+//   res.send({
+//     message: "Path Page",
+//   });
+// });
+
+// router.post("/", (req, res) => {
+//   res.send({
+//     message: "Post Page",
+//   });
+// });
+
+// router.put("/", (req, res) => {
+//   res.send({
+//     message: "Put Page",
+//   });
+// });
+
+// router.delete("/", (req, res) => {
+//   res.send({
+//     message: "Delete Page",
+//   });
+// });
+
+// router
+//   .route("/")
+//   .get((req, res) => {
+//     res.send("get");
+//   })
+//   .get((req, res) => {
+//     res.send("get");
+//   });
+
+//? Router tanımlandıktan sonra app.use() tanımlaması yaparız.
+// app.use(router);
+
+// const router = require("./routes/");
+// app.use(router);
+//* Kısa Yolu :
+// app.use(require("./routes/"));
+//* Eğer hespinin başına /test getireceksek bunun yolu :
+app.use("/test", require("./routes/"));
 /* -------------------------------------------------------------------------- */
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
