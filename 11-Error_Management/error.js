394cd078d3782fd05a081522524b5c16577f13db
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-08-05 07:09:23 +0100

    errorhandler

--- 11-Error_Management/error.js
+++ 11-Error_Management/error.js
@@ -43,13 +43,36 @@ app.get("/user/:id", function (req, res, next) {
       });
     }
   } catch (err) {
-    //* Hata oluşursa catch() çalışır ve hata err parametresi ile gönderilir.
-    //* next(error) ile hatayı errorHandler'a gönderebiliriz.
+    //? Hata oluşursa catch() çalışır ve hata err parametresi ile gönderilir.
+    //? next(error) ile hatayı errorHandler'a gönderebiliriz.
     next(err);
 
-    res.status(400).send({ error: true, message: err.message });
+    res.status(400).send({
+      error: true,
+      message: err.message,
+    });
   }
-}); 
+});
+
+/* -------------------------------------------------------------------------- */
+//- ErrorHandler 4 parametreli olmak zorunda. Hata yakalayıcı parametre 1. parametredir.
+//- ErrorHandler en sonda yer almalı (sayfanın en altında)
+
+const errorHandler = (error, req, res, next) => {
+  console.log("ErrorHandler is working.");
+
+  const statusCode = res?.errorStatusCode || 500;
+
+  res.status(statusCode).send({
+    error: true,
+    message: error.message, // Hata mesajı
+    cause: error.cause, // Hata neden oluştu ({ cause: '' })
+    stack: error.stack, // Hata orjinal çıktısı
+  });
+};
+
+// Error handler son middleware olmalı.
+app.use(errorHandler);
 
 /* -------------------------------------------------------------------------- */
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
