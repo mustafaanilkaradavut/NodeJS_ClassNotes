b38e6c3358e5b299c001109ad1078b8df89f01bc
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-08-07 07:56:09 +0100

    connection sequelize

--- 12-Todo_Sequelize_1/app.js
+++ 12-Todo_Sequelize_1/app.js
@@ -1,4 +1,52 @@
 "use strict";
-/* -------------------------------------------------------------------------- */
-//! npm i sequelize sqlite3
 
+/* -------------------------------------------------------------------------- */
+//..                 EXPRESSJS - TODO Project with Sequelize                  */
+/* -------------------------------------------------------------------------- */
+
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+
+/* -------------------------------------------------------------------------- */
+
+//__ Accept JSON data :
+
+app.use(express.json());
+
+app.all("/", (req, res) => {
+  res.send("WELCOME TODO API");
+});
+
+/* -------------------------------------------------------------------------- */
+
+//- SEQUELIZE :
+
+const { Sequelize, DataTypes } = require("sequelize");
+
+//? Connection :
+// const sequelize = new Sequelize("sqlite:./db.sqlite3");
+const sequelize = new Sequelize(
+  "sqlite:" + (process.env.SQLITE || "./db.sqlite3")
+);
+
+/* -------------------------------------------------------------------------- */
+
+//__ continue from here ...
+
+const errorHandler = (err, req, res, next) => {
+  const errorStatusCode = res.errorStatusCode ?? 500;
+  console.log("errorHandler worked.");
+  res.status(errorStatusCode).send({
+    error: true, // special data
+    message: err.message, // error string message
+    cause: err.cause, // error option cause
+    // stack: err.stack, // error details
+  });
+};
+app.use(errorHandler);
+
+/* -------------------------------------------------------------------------- */
+app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
