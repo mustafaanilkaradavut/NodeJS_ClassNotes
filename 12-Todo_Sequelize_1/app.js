6e4e0373fd093408d12163a0249dafc0be556246
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-08-08 08:00:12 +0100

    datatypes

--- 12-Todo_Sequelize_1/app.js
+++ 12-Todo_Sequelize_1/app.js
@@ -23,6 +23,7 @@ app.all("/", (req, res) => {
 /* -------------------------------------------------------------------------- */
 
 //- SEQUELIZE :
+// $ npm i sequelize sqlite3
 
 const { Sequelize, DataTypes } = require("sequelize");
 
@@ -36,18 +37,42 @@ const sequelize = new Sequelize(
 
 //! Sequelize Model:
 
+//* Her bir model veritabanında bir tabloya karşılık gelir.
 //sequelize.define("tableName", { ...colums });
 const Todo = sequelize.define("todos", {
-  id: {
-    type: DataTypes.INTEGER, //, DataType // Sütun veri tipi
-    allowNull: false, // default: true // Sütun verisi boş olabilir mi?
-    unique: true, // default: false // Benzersiz kayıt mı?
-    defaultValue: " ", // Kayıt eklendiğinde default olarak ne yazsın ?
-    // primaryKey: true, // defatult: false //, Tablonun her bir kaydını ifade eden benzersiz bir numara
-    // autoIncrement: true, // default: false // Sütun değeri her bir kayıtta otomatik olarak +1 artsın mı?
-    // comment: "We can add comment",
-    // field: "custom_field_name",
+  //, ID sütunu belirtmemize gerek yoktur. Sequelize ID sütununu otomatik oluşturur.
+  // id: {
+  //   type: DataTypes.INTEGER, //, DataType // Sütun veri tipi
+  //   allowNull: false, // default: true //, Sütun verisi boş olabilir mi?
+  //   unique: true, // default: false //, Benzersiz kayıt mı?
+  //   defaultValue: " ", //, Kayıt eklendiğinde default olarak ne yazsın ?
+  // primaryKey: true, // defatult: false //, Tablonun her bir kaydını ifade eden benzersiz bir numara
+  // autoIncrement: true, // default: false //, Sütun değeri her bir kayıtta otomatik olarak +1 artsın mı?
+  // comment: "We can add comment",
+  // field: "custom_field_name",
+  // },
+
+  title: {
+    type: DataTypes.STRING(256), // varchar(256)
+    allowNull: false,
   },
+
+  description: DataTypes.TEXT, // ShortHand
+
+  priority: {
+    // 1:High,  0:Normal,  -1:Low
+    type: DataTypes.TINYINT,
+    defaultValue: false,
+    defaultValue: 0,
+  },
+
+  isDone: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  },
+
+  //__ createAt ve updateAt tanımlamaya gerek yok. Otomatik oluşturulur.
 });
 
 /* -------------------------------------------------------------------------- */
