be1114e532d707e2e6512687262ee9cef77d8ba2
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-08-13 10:07:19 +0100

    controller

--- 15-BlogApi_Mongoose/src/controllers/blogController.js
+++ 15-BlogApi_Mongoose/src/controllers/blogController.js
@@ -2,3 +2,18 @@
 /* -------------------------------------------------------------------------- */
 //-                °    EXPRESSJS - BLOG Project with Mongoose                */
 /* -------------------------------------------------------------------------- */
+
+//? Call Models :
+const { BlogCategory, BlogPost } = require("../models/blogModel");
+
+/* -------------------------------------------------------------------------- */
+//? BlogCategory Controller :
+
+module.exports.BlogCategory = {
+  create: async (req, res) => {},
+};
+
+/* -------------------------------------------------------------------------- */
+//? BlogPost Controller :
+
+module.exports.BlogPost = {};
