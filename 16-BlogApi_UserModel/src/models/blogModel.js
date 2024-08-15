ef7af8525bc9da9644f6faa8e38ea0e634bfcf5e
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-08-15 08:40:47 +0100

    blogpost

--- 16-BlogApi_UserModel/src/models/blogModel.js
+++ 16-BlogApi_UserModel/src/models/blogModel.js
@@ -1,10 +1,10 @@
-"use strict";
+'use strict';
 /* -------------------------------------------------------------------------- */
 //-                °    EXPRESSJS - BLOG Project with Mongoose                */
 /* -------------------------------------------------------------------------- */
 //! Mongoose :
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 // const ModelName = new mongoose.Schema({ ...fields }, { ...settings });
 // const ModelSchema = new mongoose.Schema(
@@ -46,18 +46,18 @@ const mongoose = require("mongoose");
 //! BlogCategory Schema :
 
 const BlogCategorySchema = new mongoose.Schema(
-  {
-    // _id
-    name: {
-      type: String,
-      trim: true,
-      required: true,
-    },
-  },
-  {
-    collation: "blogCategories",
-    timestamps: true,
-  }
+   {
+      // _id
+      name: {
+         type: String,
+         trim: true,
+         required: true,
+      },
+   },
+   {
+      collection: 'blogCategories',
+      timestamps: true,
+   }
 );
 
 //, Set Model :
@@ -67,36 +67,36 @@ const BlogCategorySchema = new mongoose.Schema(
 //! BlogPost Schema :
 
 const BlogPostSchema = new mongoose.Schema(
-  {
-    // _id
+   {
+      // _id
 
-    categoryId: {
-      // Default Relation: ManyToOne
-      type: mongoose.Schema.Types.ObjectId, // Mongoose kütüphanesinden alırız. // Hexadecimal Format: 'fedcba9876543210'
-      ref: "BlogCategory", // ID hangi Model'e ait. (mongoose.model('ModelName', Schema))
-      required: true,
-      // unique: true, // Convert to OneToOne Relation.
-    },
+      categoryId: {
+         // Default Relation: ManyToOne
+         type: mongoose.Schema.Types.ObjectId, // Mongoose kütüphanesinden alırız. // Hexadecimal Format: 'fedcba9876543210'
+         ref: 'BlogCategory', // ID hangi Model'e ait. (mongoose.model('ModelName', Schema))
+         required: true,
+         // unique: true, // Convert to OneToOne Relation.
+      },
 
-    title: {
-      type: String,
-      trim: true,
-      required: true,
-    },
+      title: {
+         type: String,
+         trim: true,
+         required: true,
+      },
 
-    content: {
-      type: String,
-      trim: true,
-      required: true,
-    },
+      content: {
+         type: String,
+         trim: true,
+         required: true,
+      },
 
-    // createdAt // timestapms : true
-    // updatedAt // timestamps : true
-  },
-  {
-    collection: "blogPosts",
-    timestamps: true,
-  }
+      // createdAt // timestapms : true
+      // updatedAt // timestamps : true
+   },
+   {
+      collection: 'blogPosts',
+      timestamps: true,
+   }
 );
 
 //, Set Model :
@@ -105,6 +105,6 @@ const BlogPostSchema = new mongoose.Schema(
 /* -------------------------------------------------------------------------- */
 
 module.exports = {
-  BlogCategory: mongoose.model("BlogCategory", BlogCategorySchema),
-  BlogPost: mongoose.model("BlogPost", BlogPostSchema),
+   BlogCategory: mongoose.model('BlogCategory', BlogCategorySchema),
+   BlogPost: mongoose.model('BlogPost', BlogPostSchema),
 };
--- 16-BlogApi_UserModel/src/routes/blogRouter.js
+++ 16-BlogApi_UserModel/src/routes/blogRouter.js
@@ -13,7 +13,14 @@ const { BlogCategory, BlogPost } = require("../controllers/blogController");
 // URL : /blog ->
 
 // BlogCategory
-router.route("/category").post(BlogCategory.create);
+router.route("/category").get(BlogCategory.list).post(BlogCategory.create);
+
+router
+  .route("/category/:categoryId")
+  .get(BlogCategory.read)
+  .put(BlogCategory.update)
+  .patch(BlogCategory.update)
+  .delete(BlogCategory.delete);
 
 /* -------------------------------------------------------------------------- */
 module.exports = router;
