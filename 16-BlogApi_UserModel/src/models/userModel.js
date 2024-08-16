bd8d65a126011f9712d1380998c9c7fa3e5e3ea0
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-08-16 09:28:23 +0100

    encrypt

--- 16-BlogApi_UserModel/src/models/userModel.js
+++ 16-BlogApi_UserModel/src/models/userModel.js
@@ -17,6 +17,17 @@ const mongoose = require('mongoose');
 
 const crypto = require('node:crypto');
 
+//__ Parameters :
+const keyCode = process.env.SECRET_KEY; // Şifreleme anahtarı
+const loopCount = 10000; // Döngü sayısı
+const charCount = 32; // write 32 for 64
+const encType = 'sha412'; // Şifreleme algoritması
+
+//? Return encrypt password :
+const passwordEncrypt = function (password) {
+  return crypto.pbkdf2Sync(password, keyCode, loopCount, charCount, encType);
+};
+
 /* -------------------------------------------------------------------------- */
 
 const UserSchema = new mongoose.Schema(
