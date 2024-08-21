5e4cf12458684d9853077ba0b53d9c0d2dc8f86b
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-08-21 05:30:48 +0100

    useControl

--- 17-BlogApi_SessionCookies/index.js
+++ 17-BlogApi_SessionCookies/index.js
@@ -40,11 +40,17 @@ app.use(
 );
 
 /* -------------------------------------------------------------------------- */
+//, Middleware for check user data from session :
+
+app.use(require('./src/middlewares/userControl'));
+
+/* -------------------------------------------------------------------------- */
 
 app.all('/', (req, res) => {
    res.send({
       session: req.session,
       message: 'WELCOME TO BLOG API',
+      user: req.user,
    });
 });
 
