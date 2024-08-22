45ec0f7a1d051a299901389d84d8681a979367b2
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-08-22 05:16:24 +0100

    starter

---
+++ 19-PersonnelApi/index.js
@@ -0,0 +1,29 @@
+'use strict';
+/* -------------------------------------------------------------------------- */
+//-                     EXPRESSJS - BLOG Project with Mongoose                */
+/* -------------------------------------------------------------------------- */
+
+/*
+    $ npm i express dotenv mongoose express-async-errors
+    $ npm i cookie-session
+    $ npm i jsonwebtoken
+*/
+
+const express = require('express');
+const app = express();
+
+/* ------------------------------------------------------- */
+
+// continue from here...
+
+/* ------------------------------------------------------- */
+
+// errorHandler:
+app.use(require('./src/middlewares/errorHandler'));
+
+// RUN SERVER:
+app.listen(PORT, () => console.log('http://127.0.0.1:' + PORT));
+
+/* ------------------------------------------------------- */
+// Syncronization (must be in commentLine):
+// require('./src/helpers/sync')()
