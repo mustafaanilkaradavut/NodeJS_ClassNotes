60068c99410b71c3aad4fcc64795b566082d3912
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-10-27 23:15:22 +0000

    token model

--- 20-PersonnelApi_Token_Perms/src/models/token.model.js
+++ 20-PersonnelApi_Token_Perms/src/models/token.model.js
@@ -7,3 +7,30 @@ const { mongoose } = require('../configs/dbConnection');
 
 /* -------------------------------------------------------------------------- */
 
+//__ Token Model :
+
+const TokenSchema = new mongoose.Schema(
+   {
+      userId: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: 'Personnel', // Personnel.model kisminda ne isim verdik ona bakariz.
+         required: true,
+      },
+
+      token: {
+         type: String,
+         trim: true,
+         required: true,
+         unique: true,
+         index: true,
+      },
+   },
+   {
+      collection: 'tokens',
+      timestamps: true,
+   }
+);
+
+/* -------------------------------------------------------------------------- */
+
+module.exports = mongoose.model('Token', TokenSchema);
