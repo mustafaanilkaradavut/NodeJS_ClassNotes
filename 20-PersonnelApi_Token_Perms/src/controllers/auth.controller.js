210f5e125f57fd7336a15e3d337c2c7f86a7c598
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-10-28 14:23:50 +0000

    token

--- 20-PersonnelApi_Token_Perms/src/controllers/auth.controller.js
+++ 20-PersonnelApi_Token_Perms/src/controllers/auth.controller.js
@@ -12,11 +12,31 @@ module.exports = {
    login: async (req, res) => {
       const { username, password } = req.body;
 
-      if (username & password) {
+      if (username && password) {
          const user = await Personnel.findOne({ username });
 
          if (user && user.password == passwordEncrypt(password)) {
             if (user.isActive) {
+               //, TOKEN
+
+               //* Token var mi ?
+               let tokenData = await Token.findOne({ userId: user._id });
+
+               //* Token yoksa olustur:
+               if (!tokenData) {
+                  tokenData = await Token.create({
+                     userId: user._id,
+                     token: passwordEncrypt(user._id + Date.now()), // Benzersiz bir id olusturmak icin yazariz.
+                  });
+               }
+
+               res.status(200).send({
+                  error: false,
+                  token: tokenData.token,
+                  user,
+               });
+
+               //, TOKEN
             } else {
                res.errorStatusCode = 401;
                throw new Error('This user is not Active !');
