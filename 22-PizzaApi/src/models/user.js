0be9f2529a8d57139d61bd928f5b717042f769fd
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-11-12 00:43:26 +0000

    userUpdate

--- 22-PizzaApi/src/models/user.js
+++ 22-PizzaApi/src/models/user.js
@@ -6,3 +6,47 @@
 const { mongoose } = require('../configs/dbConnection');
 
 /* -------------------------------------------------------------------------- */
+
+const UserSchema = new mongoose.Schema(
+   {
+      username: {
+         type: String,
+         trim: true,
+         required: true,
+         unique: true,
+      },
+      password: {
+         type: String,
+         trim: true,
+         required: true,
+         set: (password) => passwordEncrypt(passoword),
+         // set:passwordEncrypt
+         validate: [
+            (password) =>
+               /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
+                  password
+               ),
+            'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character.',
+         ],
+      },
+      email: {
+         type: String,
+         trim: true,
+         unique: ture,
+         required: true,
+         validate: [
+            (email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email),
+            'Please fill a valid email address.',
+         ],
+      },
+      isActive: {
+         type: Boolean,
+         default: true,
+      },
+      isAdmin: {
+         type: Boolean,
+         default: true,
+      },
+   },
+   { collection: 'users', timestamps: true }
+);
