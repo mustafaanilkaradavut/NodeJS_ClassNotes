3748be38f3e4e5ca6f747114bde8ae4ff854444f
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-11-14 04:35:06 +0000

    multer

--- 24-PizzaApi_Email_Upload/src/routes/pizza.js
+++ 24-PizzaApi_Email_Upload/src/routes/pizza.js
@@ -4,15 +4,41 @@
 /* -------------------------------------------------------------------------- */
 
 const router = require('express').Router();
+/* -------------------------------------------------------------------------- */
 
-/* ------------------------------------------------------- */
+// UPLOAD (Multer Middleware)
+// npm i multer
+// https://expressjs.com/en/resources/middleware/multer.html
+
+const multer = require('multer');
+
+const upload = multer({
+   // dest: './uploads',
+   storage: multer.diskStorage({
+      destination: './uploads',
+      filename: function (req, file, returnCallback) {
+         console.log('file', file);
+         // returnCallback(error, fileName);
+         // returnCallback(null, 'pizzaShop.jpg');
+         // returnCallback(null, file.originalname);
+         returnCallback(null, Date.now() + '_' + file.originalname);
+      },
+   }),
+});
+
+/* -------------------------------------------------------------------------- */
 //.. routes/pizza:
 
 const pizza = require('../controllers/pizza');
 
 //.. URL: /pizzas
 
-router.route('/').get(pizza.list).post(pizza.create);
+router
+   .route('/')
+   .get(pizza.list)
+   // .post(upload.single('image'), pizza.create);
+   .post(upload.array('image'), pizza.create);
+// .post(upload.any(), pizza.create);
 
 router
    .route('/:id')
@@ -21,5 +47,5 @@ router
    .patch(pizza.update)
    .delete(pizza.delete);
 
-/* ------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
 module.exports = router;
