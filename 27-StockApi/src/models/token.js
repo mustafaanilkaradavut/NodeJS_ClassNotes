493d36f7741536136edf92dc0a930a565263c646
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-12-22 23:10:41 +0000

    token

--- 27-StockApi/src/models/token.js
+++ 27-StockApi/src/models/token.js
@@ -1,38 +1,41 @@
-"use strict"
-/* -------------------------------------------------------
-    | FULLSTACK TEAM | NODEJS / EXPRESS |
-------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+'use strict';
+/* -------------------------------------------------------------------------- */
+//-                 | FULLSTACK TEAM | NODEJS / EXPRESS |                     */
+/* -------------------------------------------------------------------------- */
+
+const { mongoose } = require('../configs/dbConnection');
 /* ------------------------------------------------------- *
 {
-    "userId": "65343222b67e9681f937f001",
-    "token": "...tokenKey..."
-  }
-  /* ------------------------------------------------------- */
-  // Token Model:
+   "userId": "65343222b67e9681f937f001",
+   "token": "...tokenKey..."
+}
+/* ------------------------------------------------------- */
 
-  const TokenSchema = new mongoose.Schema({
+//! Token Model:
 
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
-        index: true,
-        unique: true,
-    },
+const TokenSchema = new mongoose.Schema(
+   {
+      userId: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: 'User',
+         required: true,
+         index: true,
+         unique: true,
+      },
 
-    token: {
-        type: String,
-        trim: true,
-        required: true,
-        index: true,
-        unique: true,
-    }
+      token: {
+         type: String,
+         trim: true,
+         required: true,
+         index: true,
+         unique: true,
+      },
+   },
+   {
+      collection: 'tokens',
+      timestamps: true,
+   }
+);
 
-  }, {
-    collection: 'tokens',
-    timestamps: true
-  })
-
-  /* ------------------------------------------------------- */
-  module.exports = mongoose.model('Token', TokenSchema)
+/* ------------------------------------------------------- */
+module.exports = mongoose.model('Token', TokenSchema);
